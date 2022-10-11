32ac4ab3a5ed91b87ebb2cafa0c71f0dc0044d64
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-12 00:21:07 +0530

    routing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +8,28 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { ViewAirlinesComponent } from './view-airlines/view-airlines.component';
 import { ViewPassengersComponent } from './view-passengers/view-passengers.component';
 
+const appRoutes:Routes=[
+ {
+  path:"",component:ViewAirlinesComponent
+ },
+ {
+  path:"view",component:ViewPassengersComponent
+ }
+]
+
+
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ViewAirlinesComponent,
     ViewPassengersComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
