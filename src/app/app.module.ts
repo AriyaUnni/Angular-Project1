28c40718dcfe8020a25aa2d1ed961a4128acb671
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-12 00:01:01 +0530

    passenger page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ViewAirlinesComponent } from './view-airlines/view-airlines.component';
+import { ViewPassengersComponent } from './view-passengers/view-passengers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ViewAirlinesComponent
+    ViewAirlinesComponent,
+    ViewPassengersComponent
   ],
   imports: [
     BrowserModule,
